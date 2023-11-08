39cd0e44dd220b4e957adc7092a73e92cc70b5be
Sergey Vasilyev <nolar@nolar.info>
2023-11-09 00:17:32 +0100

    Implement the initial functionality
    Signed-off-by: Sergey Vasilyev <nolar@nolar.info>

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 # http://www.sphinx-doc.org/en/master/config
 import os
+import types
 
 import docutils.nodes
 
@@ -46,6 +47,7 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'mypy': ('https://mypy.readthedocs.io/en/latest/', None),
     'kopf': ('https://kopf.readthedocs.io/en/latest/', None),
+    'aiohttp': ('https://docs.aiohttp.org/en/stable/', None),
 }
 
 
@@ -80,17 +82,31 @@ def run_apidoc(_):
         os.path.join(root_path, 'kmock'),
     ] + ignore_paths
 
-    try:
-        # Sphinx 1.7+
-        from sphinx.ext import apidoc
-        apidoc.main(argv)
-    except ImportError:
-        # Sphinx 1.6 (and earlier)
-        from sphinx import apidoc
-        argv.insert(0, apidoc.__file__)
-        apidoc.main(argv)
+    from sphinx.ext import apidoc
+    apidoc.main(argv)
+
+
+# KMock exposes most public classes/types on the `kmock` fixture itself (RawHandler & descendants),
+# so that there is no need to use `from kmock import blah` syntax and the old `kmock.blah` remains.
+# But we do not want these re-exposed members of the `kmock` fixtures to be documented as public
+# members of the handlers, since they are already documented as the members of the `kmock` module.
+# (Classes are more precise due to their source module; type aliases/generics are excluded blindly.)
+def autodoc_skip_member(app, what, name, obj, skip, options):
+    from typing import Union
+    from types import GenericAlias
+
+    # Rare cases like clusterwide(…), namespace(…), etc, which return Criteria() instances.
+    # Alternative: remake them to classes with the overridden __new__(), which returns K8sCriteria.
+    if what == 'class' and isinstance(obj, types.FunctionType):
+        if not name.startswith('_') and '.' not in obj.__qualname__:
+            return True
+    if what == 'class' and isinstance(obj, type) and obj.__module__.startswith('kmock.'):
+        return True
+    if what == 'class' and isinstance(obj, GenericAlias | Union):
+        return True
 
 
 def setup(app):
     app.add_crossref_type('kwarg', 'kwarg', "pair: %s; kwarg", docutils.nodes.literal)
     app.connect('builder-inited', run_apidoc)
+    app.connect('autodoc-skip-member', autodoc_skip_member)
--- docs/index.rst
+++ docs/index.rst
@@ -2,9 +2,9 @@
 HTTP/API/Kubernetes Mock Server
 ===============================
 
-KMock is a dummy/mock server to mimick the Kubernetes API or any other API.
+KMock is a dummy/mock server to mimick the Kubernetes API, any other API, or bare HTTP servers.
 
-Usually, the developer populates the server with K8s resources, raw request patterns, and supposed responses, then uses the server via **any** HTTP client, be that in the same framework in Python or something external (e.g. ``curl``, even ``kubectl``). In the end, the developer asserts which endpoints were called, with which specific requests, how many times, so on — thus verifying that the client works as intended.
+Usually, the developer populates the server with Kubernetes resources, raw request patterns, and supposed responses, then uses the server via **any** HTTP client of choice, be that in the same framework in Python or something external (e.g. ``curl``, even ``kubectl``). In the end, the developer asserts which endpoints were called, with which specific requests, how many times, so on — thus verifying that the developed client works as intended.
 
 KMock runs well with looptime_, the time dilation/contraction library for asyncio & pytest.
 
@@ -14,4 +14,18 @@ KMock runs well with looptime_, the time dilation/contraction library for asynci
    :maxdepth: 2
    :caption: Tutorial:
 
-   contributing
+   intro
+   usage
+   requests
+   responses
+   streams
+   assertions
+   ideas
+   scope
+   linters
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Reference:
+
+   packages/kmock
