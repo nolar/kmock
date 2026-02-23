eb89bb23e0ac23dedf8b0efd312df36debf178bd
Sergey Vasilyev <nolar@nolar.info>
2026-02-23 13:09:00 +0100

    Extract dict-manipulating routines into a new module
    It will be extended with extra functionality in the next commit.
    
    Signed-off-by: Sergey Vasilyev <nolar@nolar.info>

--- kmock/_internal/k8s_dicts.py
+++ kmock/_internal/k8s_dicts.py
@@ -1,10 +1,11 @@
 import collections.abc
 import copy
 from collections.abc import Iterable, Iterator, Mapping, MutableMapping, MutableSequence
-from types import EllipsisType
 from typing import Any, overload
 
 import attrs
+
+from kmock._internal import dicts
 
 # A note for future self: if you ever want to turn these classes into generics,
 # forget it — it will not work. We rely on recursive processing of nested dicts
@@ -14,75 +15,6 @@ import attrs
 # Even if it were possible, there is no benefit: the classes are used for one
 # and only one purpose — the Kubernetes object accessors. There is no reuse.
 # As such, the dicts remain bound to their purpose of K8s objects: [str, Any].
-
-
-def patch_dict(value: Mapping[str, Any], patch: Mapping[str, Any], /, **kwargs: Any) -> Mapping[str, Any]:
-    """
-    Patch & merge the dicts recursively. ``None`` means the deletion of the key.
-
-    The order of the keys is preserved from the value in the first place,
-    then from the patch for the new keys. There is no re-shuffling of the keys.
-
-    The patching is memory-thrifty and follows the "copy-on-write" principle.
-    I.e., the original values are used without copying if there are no changes
-    (even the mutable sub-dicts). New dicts are made only if there are changes.
-
-    Lists and other non-mapping values are taken without merging.
-    """
-    result: dict[str, Any] = {}
-    patch = dict(patch, **kwargs)  # it is safe to loose the runtime type here
-    keys = list(value) + [key for key in patch if key not in value]
-    for key in keys:
-        a: Any | None = value.get(key)
-        b: Any | None = patch.get(key)
-        match a, b:
-            case _, _ if key not in patch:  # old unaffected keys
-                result[key] = value[key]
-            case _, _ if patch[key] is None:  # deleted keys
-                pass
-            case _, collections.abc.Mapping() if key not in value:  # new appended keys
-                result[key] = patch_dict({}, patch[key])
-            case _, _ if key not in value:  # new appended keys
-                result[key] = patch[key]
-            case collections.abc.Mapping(), collections.abc.Mapping():
-                result[key] = patch_dict(a, b)
-            case collections.abc.Mapping(), _:
-                raise ValueError(f"Cannot patch a dict by a scalar: {a!r} << {b!r}")
-            case _, collections.abc.Mapping():
-                raise ValueError(f"Cannot patch a scalar by a dict: {a!r} << {b!r}")
-            case _:  # overwrite without merging
-                result[key] = b
-    return result
-
-
-def match_dict(value: Mapping[str, Any], pattern: Mapping[str, Any], /, *, strict: bool) -> bool:
-    """
-    Check if the dict matches a pattern recursively.
-
-    All keys in the pattern must match. Extra keys can exist in the dict.
-    Ellipsis (``...``) in the pattern is a placeholder for any present value.
-    For the absent keys, use the ``...`` pattern with a negation of the result.
-    In particular, ``None`` means that the key is present and stores ``None``.
-    """
-    required_keys = set(pattern)
-    available_keys = set(value)
-    if required_keys - available_keys:
-        return False  # some required keys are missing
-    if strict and available_keys - required_keys:
-        return False  # some extra keys are present, but not allowed
-    for key in  required_keys | (available_keys if strict else set()):
-        a = value[key]
-        b = pattern[key]
-        match a, b:
-            case collections.abc.Mapping(), collections.abc.Mapping():
-                if not match_dict(a, b, strict=strict):
-                    return False  # recursively missing keys
-            case _, EllipsisType():  # key is present, value is ignored
-                pass  # we already checked that the key exists
-            case _ if a != b:  # mismatching types or unequal values
-                return False
-    return True
-
 
 @attrs.define(repr=False, init=False, order=False, eq=False)
 class ObjectVersion(MutableMapping[str, Any]):
@@ -199,7 +131,7 @@ class ObjectVersion(MutableMapping[str, Any]):
     def __ge__(self, other: Mapping[str, Any]) -> bool:
         match other:
             case collections.abc.Mapping():
-                return match_dict(self, other, strict=False)
+                return dicts.match_dict(self, other, strict=False)
             case _:
                 return NotImplemented
 
@@ -208,7 +140,7 @@ class ObjectVersion(MutableMapping[str, Any]):
     def __le__(self, other: Mapping[str, Any]) -> bool:
         match other:
             case collections.abc.Mapping():
-                return match_dict(other, self, strict=False)  # we are the pattern!
+                return dicts.match_dict(other, self, strict=False)  # we are the pattern!
             case _:
                 return NotImplemented
 
@@ -237,7 +169,7 @@ class ObjectVersion(MutableMapping[str, Any]):
     def __eq__(self, other: object) -> bool:
         match other:
             case collections.abc.Mapping():
-                return match_dict(self, other, strict=True)
+                return dicts.match_dict(self, other, strict=True)
             case _:
                 return NotImplemented
 
@@ -570,7 +502,7 @@ class Object(MutableMapping[str, Any]):
             self.__history.append(ObjectVersion(changes, **kwargs))
         else:
             old_version = copy.deepcopy(self.last.raw)  # avoid side mutations
-            new_version = patch_dict(old_version, changes or {}, **kwargs)
+            new_version = dicts.patch_dict(old_version, changes or {}, **kwargs)
             self.__history.append(ObjectVersion(new_version))
 
     @property
